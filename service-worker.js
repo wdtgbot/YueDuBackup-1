b8b5ea7a797e32075668943bd5696ee3da8c93af
ifwlzs <ifwlzs@users.noreply.github.com>
2021-09-25 11:56:33 +0000

    Update tool.html 701ee701ea27bf5337d2164ade864a6c042241b5

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "1c7d44610fcd0d959ee2a3e9523a986f"
+    "revision": "167aecc7bdca8a902dd0d7bb35447a96"
   },
   {
     "url": "assets/css/0.styles.e42cd381.css",
@@ -38,68 +38,68 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.0ecd01da.js",
-    "revision": "755acc7251fbc413222b879f64472c06"
+    "url": "assets/js/10.51730ba3.js",
+    "revision": "d897509aab14d165bc8e024a670447b2"
   },
   {
-    "url": "assets/js/11.eb495b8b.js",
-    "revision": "bf1701f4219124a09ad0b779bf2854a5"
+    "url": "assets/js/11.f6569806.js",
+    "revision": "f64706110c41c2ecf43631d983cd1d27"
   },
   {
-    "url": "assets/js/12.b7c4bb27.js",
-    "revision": "88a87a6bfe8df4b0bbaf7d0219c9fe85"
+    "url": "assets/js/12.d075e12d.js",
+    "revision": "f1fb3ca541198d41f43ad7702e84c29f"
   },
   {
-    "url": "assets/js/13.968a9fe2.js",
-    "revision": "d1ff4ff95870c8b26a80df6645c0bc0f"
+    "url": "assets/js/13.1700e0f0.js",
+    "revision": "fb1909913ac560351dc03be378387dba"
   },
   {
-    "url": "assets/js/14.e29b318c.js",
-    "revision": "bafab39f6afb91c19f7889449be4d73a"
+    "url": "assets/js/14.0ad55cae.js",
+    "revision": "2e87fc214254c242a19e1aebf622cdd0"
   },
   {
-    "url": "assets/js/15.cf48d6ab.js",
-    "revision": "500c268e3eea8cc26978a427f68848b9"
+    "url": "assets/js/15.e6340a0d.js",
+    "revision": "1e7e13dcd325daffa59266ed26a593f9"
   },
   {
-    "url": "assets/js/16.e1649402.js",
-    "revision": "e6eb75077820a04cd7dca013ffbaab1d"
+    "url": "assets/js/16.c72bc87d.js",
+    "revision": "6905e644351b5aa151d3bb0e54ae20fe"
   },
   {
-    "url": "assets/js/2.07710c87.js",
-    "revision": "d21c9adaa4832b0dd43482ad0a31a068"
+    "url": "assets/js/2.7a99f9ec.js",
+    "revision": "4f6e385f6d202522e25de511f3147a17"
   },
   {
-    "url": "assets/js/3.eae12c36.js",
-    "revision": "646092c6b5f3427257b4cee6bb9c5e73"
+    "url": "assets/js/3.0eed76f5.js",
+    "revision": "64fa3481a2611a6d3c32de27d74574fb"
   },
   {
-    "url": "assets/js/4.21c9c96d.js",
-    "revision": "ffe996daa2a697862967616db24d9cc4"
+    "url": "assets/js/4.467ce04f.js",
+    "revision": "b350adc5554ac5e5c25747e70eb4246a"
   },
   {
-    "url": "assets/js/5.930ec43e.js",
-    "revision": "be09ac676a5a6af36e57e1d8e8494d77"
+    "url": "assets/js/5.40cd9914.js",
+    "revision": "54ecbcb6ddd3cb99dc4ca4ae3adfff88"
   },
   {
-    "url": "assets/js/6.2e24ea78.js",
-    "revision": "76df341dde683d4de904768f256c688f"
+    "url": "assets/js/6.ef852fc5.js",
+    "revision": "dc1d74b71fc1188c15810dacd8346818"
   },
   {
-    "url": "assets/js/7.d81ee1bf.js",
-    "revision": "cdaafeb867cc48408282edc4dc397e32"
+    "url": "assets/js/7.4327e7b2.js",
+    "revision": "9b7eb6fbaa89d7952a2b33a5ed1e89e3"
   },
   {
-    "url": "assets/js/8.09067e7b.js",
-    "revision": "07ff64b23b7a694fdb02e5839f73356f"
+    "url": "assets/js/8.2cc0a9ff.js",
+    "revision": "518959c553845d1d118f4192b4cac5b6"
   },
   {
-    "url": "assets/js/9.2da47ffa.js",
-    "revision": "1d9f4f0a6e41ff6784476bdc7bcee8cc"
+    "url": "assets/js/9.4f60cc8b.js",
+    "revision": "7ddd4db42ae9dcd9dbf9307c6443cbf3"
   },
   {
-    "url": "assets/js/app.9dd5c6ee.js",
-    "revision": "6c98aba2e917464eff3389574f6d173a"
+    "url": "assets/js/app.f80ca4fb.js",
+    "revision": "fdab3b856ca1b05051dcba2d964661d9"
   },
   {
     "url": "icons/icon-128x128.png",
@@ -135,35 +135,35 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "8c940b19f564c49e2823bed6042baeed"
+    "revision": "d8fce61005d8e37af1a7ab296ac77052"
   },
   {
     "url": "yuedu/index.html",
-    "revision": "0ff4e09853b6c66d1a790004c9224da3"
+    "revision": "76486cc51f88090869e8597cca8501c2"
   },
   {
     "url": "yuedu/个人备份说明.html",
-    "revision": "1bf849a27f6023a3e1d832302dd7114c"
+    "revision": "55f82340ae1c94e79ff8df657f81de49"
   },
   {
     "url": "yuedu/书源校验工具.html",
-    "revision": "0c35a32edcc9317f02c4fdc93030e423"
+    "revision": "443f0a7b2a8d72c6dcf7b67cdb55d150"
   },
   {
     "url": "yuedu/关耳美化发现工具.html",
-    "revision": "269c72744750c36fc3aa4f3695e5b955"
+    "revision": "f84009da64f5f7e341bac9edf6cc982e"
   },
   {
     "url": "yuedu/备份文件说明.html",
-    "revision": "1b78babb7c038c8386ccca1cf9643151"
+    "revision": "c9be142f500063df8d465e9a219b5604"
   },
   {
     "url": "yuedu/教程.html",
-    "revision": "50bb051b086e980212e355926907f791"
+    "revision": "228d849897a2eaf05b3e4ca41ad226e2"
   },
   {
     "url": "yuedu/资源共享.html",
-    "revision": "078a7aef25526d8795b037b1e4beb493"
+    "revision": "37dd74339240e4b3e1dc6cc17c110232"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
